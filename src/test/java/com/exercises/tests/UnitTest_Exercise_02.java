43197ff34e9b863ec70c2912e8de1fe4ed487e23
vunguyen <vunht@onemount.com>
2021-10-28 22:09:20 +0700

    Update

--- src/test/java/com/exercises/tests/UnitTest_Exercise_02.java
+++ src/test/java/com/exercises/tests/UnitTest_Exercise_02.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class UnitTest_Exercise_02 {
@@ -37,6 +39,25 @@ public class UnitTest_Exercise_02 {
     public void shouleBeFalseOutlookValid() {
         String email = "vunguyen1993@yahoo.com";
         Assertions.assertFalse(Exercise_02.matchEmailOutlook(email));
+    }
+
+    @Test
+    @DisplayName("Check hàm kiểm tra regex match với email là @gmail hoặc @outlook")
+    public void shouleBeTrueGmailOrOutlookValid() {
+        List<String> listEmail = Arrays.asList("vunguyen1993@outlook.com", "vunguyen1993@gmail.com", "vunguyen@gmail.com");
+        for (String email : listEmail) {
+            Assertions.assertTrue(Exercise_02.matchEmailGmailOrOutlook(email));
+        }
+
+    }
+
+    @Test
+    @DisplayName("Check hàm kiểm tra regex không match với email là @gmail hoặc @outlook")
+    public void shouleBeFalseGmailOrOutlookValid() {
+        List<String> listEmail = Arrays.asList("vunguyen1993@gg.com", "vunguyen1993@onemount.com", "vunguyen1993@yahoo.com");
+        for (String email : listEmail) {
+            Assertions.assertFalse(Exercise_02.matchEmailGmailOrOutlook(email));
+        }
     }
 
 
